32723e167eb39622bc4714da3654e7ba297fda9c
EverlynAsiko <asikoeverlyn@gmail.com>
2018-10-14 21:49:05 +0300

    Feature:Added map api key and responsivity

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiZXZlcmx5bmFzaWtvIiwiYSI6ImNqbjh4czU4bDFsaDMza29jc2Rmdzg3NmgifQ.zljz-wlp0sivYWVzLSBCNg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
